f33fb436d1089c33920ed9dbc22630b6b46603c5
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2025-11-13 13:44:17 +0530

    Update run_first.sh

--- run_first.sh
+++ run_first.sh
@@ -24,7 +24,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt update -q
 apt upgrade -yq
 add-apt-repository universe
-apt install -yq curl postgresql lazydocker postgresql-contrib git snapd python3 python3-pip pipx python3-venv fuse tmate libfuse2 ufw dnsutils fastfetch net-tools htop btop nvtop grub-customizer gparted network-manager tlp tlp-rdw linux-headers-$(uname -r)
+apt install -yq curl postgresql lazydocker postgresql-contrib git snapd python3 python3-pip pipx python3-venv fuse tmate libfuse2 ufw dnsutils fastfetch net-tools htop btop network-manager tlp tlp-rdw linux-headers-$(uname -r)
 pipx ensurepath
 
 # Install Other Stuff
@@ -34,15 +34,13 @@ echo "--------------------------------------------------------------------------
 sleep 0.5
 curl -LsSf https://astral.sh/uv/install.sh | sh                                                          # UV
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh                     # zoxide
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash                          # nvm
-nvm install node
 
 # Install LazyDocker
 echo "---------------------------------------------------------------------------------"
 echo "                         Installing from SNAP Store                              "
 echo "---------------------------------------------------------------------------------"
 sleep 0.5
-snap install snap-store postman ngrok mpv zoom-client rpi-imager PyCharm-community notepad-plus-plus
+snap install ngrok
 # snap install docker
 
 
@@ -66,46 +64,6 @@ git config --global user.name "dhimanparas20"
 git config --global user.email "dhimanparas20@gmail.com"
 git config --global credential.helper cache
 git config --global credential.helper store\]
-
-
-echo "---------------------------------------------------------------------------------"
-echo "                              Installing GRUB THEME                              "
-echo "---------------------------------------------------------------------------------"
-sleep 0.5
-git clone https://github.com/yeyushengfan258/Matrix-grub-theme.git
-cd Matrix-grub-theme
-chmod +x install.sh
-./install.sh -t window
-cd ..
-rm -rf Matrix-grub-theme
-
-echo "---------------------------------------------------------------------------------"
-echo "                          Starting of installed Packages                         "
-echo "---------------------------------------------------------------------------------"
-sleep 0.5
-echo "===============================| TLP |============================================"
-# nano /etc/tlp.conf
-systemctl start tlp
-systemctl enable tlp
-systemctl enable tlp-sleep
-systemctl restart tlp
-status tlp
-tlp-stat -s
-echo "=========================| PLATFORM TOOLS |========================================"
-wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
-unzip -o platform-tools-latest-linux.zip
-mv -f platform-tools /opt/
-ln -sf /opt/platform-tools/adb /usr/local/bin/adb
-ln -sf /opt/platform-tools/fastboot /usr/local/bin/fastboot
-echo "=========================| ARDUINO IDE |========================================"
-wget https://downloads.arduino.cc/arduino-ide/arduino-ide_2.3.6_Linux_64bit.AppImage
-chmod +x arduino-ide_2.3.6_Linux_64bit.AppImage
-mv arduino-ide_2.3.6_Linux_64bit.AppImage /opt/
-ln -sf /opt/arduino-ide_2.3.6_Linux_64bit.AppImage /usr/local/bin/arduino-ide
-echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="2341", GROUP="plugdev", MODE="0666"' | sudo tee /etc/udev/rules.d/99-arduino.rules
-echo "=========================| Black Binary |========================================"
-pipx install black
-which black
 
 
 # Clone Full Repo
