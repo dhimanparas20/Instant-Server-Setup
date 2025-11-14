b6f71f56ac70a5580e6b8ab4f9e8ab4ac10b18ad
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2025-11-14 17:58:13 +0530

    Update run_first.sh

--- Ubuntu/run_first.sh
+++ Ubuntu/run_first.sh
@@ -1,80 +1,82 @@
-#!/bin/zsh
-# The script must be run using sudo
-# The script also sets up some configurations wherever needed
+#!/usr/bin/env zsh
+set -euo pipefail
 
-# Check if the script is run with sudo
+# Error handler for zsh (ZERR instead of bash's ERR)
+trap 'echo -e "\e[31mError occurred at line $LINENO. Exiting.\e[0m"' ZERR
+
+GIT_USER="dhimanparas20"
+GIT_EMAIL="dhimanparas20@gmail.com"
+
 if [ "$(id -u)" -ne 0 ]; then
-    echo "Please run this script with sudo:"
-    echo "sudo zsh run_first.sh"
+    echo -e "\e[31mPlease run this script with sudo:\e[0m"
+    echo "sudo $0"
     exit 1
 fi
 
-# Start
 clear
-echo "---------------------------------------------------------------------------------"
-echo "                            Installing Dependencies                              "
-echo "---------------------------------------------------------------------------------"
+echo -e "\e[34m---------------------------------------------------------------------------------\e[0m"
+echo -e "\e[34m                            Installing Dependencies                              \e[0m"
+echo -e "\e[34m---------------------------------------------------------------------------------\e[0m"
 sleep 0.5
-set -e
 
-# Disable interactive prompts
 export DEBIAN_FRONTEND=noninteractive
 
-# The Basics
 apt update -q
 apt upgrade -yq
-add-apt-repository universe
-apt install -yq curl postgresql postgresql-contrib git uidmap python3 python3-pip pipx python3-venv fuse tmate libfuse2 ufw dnsutils neofetch net-tools htop btop network-manager tlp tlp-rdw linux-headers-$(uname -r)
-apt install snapd -y
+add-apt-repository -y universe
+
+PKGS=(
+    curl postgresql  postgresql-contrib git uidmap snapd python3 python3-pip pipx python3-venv
+    fuse tmate libfuse2 ufw dnsutils neofetch net-tools htop btop network-manager tlp tlp-rdw
+    software-properties-common "linux-headers-$(uname -r)"
+)
+
+for pkg in "${PKGS[@]}"; do
+    if ! dpkg -s "$pkg" &>/dev/null; then
+        apt install -yq "$pkg"
+    fi
+done
+
 pipx ensurepath
 
-# Install Other Stuff
-echo "---------------------------------------------------------------------------------"
-echo "                            Installing Other Stuff                               "
-echo "---------------------------------------------------------------------------------"
+echo -e "\e[34m---------------------------------------------------------------------------------\e[0m"
+echo -e "\e[34m                            Installing Other Stuff                               \e[0m"
+echo -e "\e[34m---------------------------------------------------------------------------------\e[0m"
 sleep 0.5
-curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | zsh #Lazydocker
-curl -LsSf https://astral.sh/uv/install.sh | sh                                                          # UV
-curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | zsh                     # zoxide
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
+curl -LsSf https://astral.sh/uv/install.sh | bash
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 
-# Install LazyDocker
-echo "---------------------------------------------------------------------------------"
-echo "                         Installing from SNAP Store                              "
-echo "---------------------------------------------------------------------------------"
+echo -e "\e[34m---------------------------------------------------------------------------------\e[0m"
+echo -e "\e[34m                         Installing from SNAP Store                              \e[0m"
+echo -e "\e[34m---------------------------------------------------------------------------------\e[0m"
 sleep 0.5
-snap install ngrok
-# snap install docker
+snap install ngrok docker
 
+# echo -e "\e[34m---------------------------------------------------------------------------------\e[0m"
+# echo -e "\e[34m                                 Installing Docker                               \e[0m"
+# echo -e "\e[34m---------------------------------------------------------------------------------\e[0m"
+# sleep 0.5
+# wget -q https://get.docker.com -O install-docker.sh
+# chmod +x install-docker.sh
+# zsh install-docker.sh
+# rm install-docker.sh
 
-# Install Docker
-echo "---------------------------------------------------------------------------------"
-echo "                                 Installing Docker                               "
-echo "---------------------------------------------------------------------------------"
+# Add user to docker group
+# groupadd -f docker
+# usermod -aG docker "$SUDO_USER"
+
+echo -e "\e[34m---------------------------------------------------------------------------------\e[0m"
+echo -e "\e[34m                                 Adding Git Configs                              \e[0m"
+echo -e "\e[34m---------------------------------------------------------------------------------\e[0m"
 sleep 0.5
-wget -q https://get.docker.com -O install-docker.sh
-chmod +x install-docker.sh
-./install-docker.sh
-rm install-docker.sh 
 
-
-# Store git passwords and add user signature
-echo "---------------------------------------------------------------------------------"
-echo "                                 Adding Git Configs                              "
-echo "---------------------------------------------------------------------------------"
-sleep 0.5
-git config --global user.name "dhimanparas20"
-git config --global user.email "dhimanparas20@gmail.com"
+git config --global user.name "$GIT_USER"
+git config --global user.email "$GIT_EMAIL"
 git config --global credential.helper cache
-git config --global credential.helper store\]
+git config --global credential.helper store
 
-
-# Finally Rebooting
-echo "---------------------------------------------------------------------------------"
-echo "                                    Reboot                                       "
-echo "---------------------------------------------------------------------------------"
-echo "after reboot proceed to 2nd step"
-#groupadd docker
-#usermod -aG docker $USER
-#newgrp docker
+echo -e "\e[34m---------------------------------------------------------------------------------\e[0m"
+echo -e "\e[34m                                    DONE                                         \e[0m"
+echo -e "\e[34m---------------------------------------------------------------------------------\e[0m"
 sleep 1
-reboot
