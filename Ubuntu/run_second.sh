28fc6c77ffb92f600ae525f3fad9f7956fb2de8f
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2025-11-14 12:34:02 +0530

    Update run_second.sh

--- Ubuntu/run_second.sh
+++ Ubuntu/run_second.sh
@@ -16,6 +16,7 @@ echo "--------------------------------------------------------------------------
 sleep 0.5
 dockerd-rootless-setuptool.sh install 
 
+
 # Install Zsh plugins
 echo "---------------------------------------------------------------------------------"
 echo "                         Cloning ohmyzsh extentions                              "
@@ -27,13 +28,13 @@ git clone https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/
 git clone https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
 sudo git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
-# Define the file path for .zshrc
-ZSHRC_FILE="$HOME/.zshrc"
 
 echo "---------------------------------------------------------------------------------"
 echo "                                   Editing .zshrc                                "
 echo "---------------------------------------------------------------------------------"
 sleep 0.5
+# Define the file path for .zshrc
+ZSHRC_FILE="$HOME/.zshrc"
 
 # Replace the line that starts with 'plugins=' to add new plugins
 sed -i 's/^plugins=(git)/plugins=(git sudo history encode64 copypath zsh-autosuggestions zsh-completions zsh-autocompletecommand-not-found extract docker colored-man-pages alias-finder zsh-syntax-highlighting)/' "$ZSHRC_FILE"
@@ -41,17 +42,18 @@ sed -i 's/^plugins=(git)/plugins=(git sudo history encode64 copypath zsh-autosug
 # Replace the line that starts with 'ZSH_THEME="roborussel"' with 'ZSH_THEME="bira"'
 sed -i 's/^ZSH_THEME="robbyrussell"/ZSH_THEME="bira"/' "$ZSHRC_FILE"
 
-# Adding Docker Alias
-if [[ -f dockerAlias.sh ]]; then
-    cat dockerAlias.sh >> "$HOME/.zshrc"
-    rm -f dockerAlias.sh
-fi
+
+echo "---------------------------------------------------------------------------------"
+echo "                                Setting Up Aliases                               "
+echo "---------------------------------------------------------------------------------"
+sleep 0.5
+cat dockerAlias.sh >> "$HOME/.zshrc"
 
 # Reload the .zshrc file to apply the changes immediately
 source "$ZSHRC_FILE"
 
+
 echo "---------------------------------------------------------------------------------"
-echo "Docker-related commands, aliases, and plugins added successfully to .zshrc."
 echo "Please manually reboot the system once, using 'sudo reboot' "
 echo "You can delete This file After Reboot."
 echo "---------------------------------------------------------------------------------"
