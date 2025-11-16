b6c42d3bf8371387c0b6cb3d8cd2e99364f9e7e7
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2025-11-16 12:42:23 +0530

    Update run_optional.sh

--- Ubuntu/run_optional.sh
+++ Ubuntu/run_optional.sh
@@ -21,6 +21,7 @@ export DEBIAN_FRONTEND=noninteractive
 sudo add-apt-repository -y ppa:danielrichter2007/grub-customizer
 sudo apt update -q
 sudo apt install -yq grub-customizer nvtop gparted unzip libfuse2 fuse
+sudo apt autoremove -y
 echo -e "\n\e[32m| DONE |\e[0m\n"
 
 
